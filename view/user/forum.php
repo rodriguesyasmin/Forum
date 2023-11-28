ca136210d7a12d21c2a7905ccf480bbeb5962223
Yasmin Rodrigues <yasminbresilienne@hotmail.com>
2023-11-28 18:35:50 -0500

    afficher articles

--- view/user/forum.php
+++ view/user/forum.php
@@ -1,4 +1,8 @@
 <?php
+require_once('lib/connex.php');
+$sql = "SELECT * FROM forum limit 5";
+$result = mysqli_query($connex, $sql);
+
 require_once('lib/checkSession.php');
 if (!$_SESSION) {
 ?>
@@ -14,3 +18,21 @@ if (!$_SESSION) {
 
 
     Aqui vai ser o forum
+
+
+    <?php
+    foreach ($result as $row) {
+
+    ?>
+
+        <div>
+            <div><?= $row['titre']; ?></div>
+            <div><?= $row['article']; ?></div>
+            <div><?= $row['dateArticle']; ?></div>
+            <div><?= $row['author']; ?></div>
+            <div class="invisible"><?= $row['utilisateur_id']; ?></div>
+
+        </div>
+    <?php
+    }
+    ?>
